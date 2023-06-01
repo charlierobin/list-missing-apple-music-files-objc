e478a942a021413a5b2eeae5a34e11ad4a6e16d9
charlierobin <charlie@charlierobin.com>
2023-06-01 16:41:14 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# list-missing-apple-music-files-objc
- 
+# An Objective-C/Foundation command line app to list missing Music files
+
+(Basically the same as [this](https://github.com/charlierobin/list-missing-apple-music-files-swift), but implemented in Objective-C and not Swift.)
+
+Because this one was the first one I wrote, it’s complete, whereas the Swift version is still not quite complete.
+
+If you run this app in the Terminal with no arguments, it tries to connect to your Music library via `ITLibrary` `libraryWithAPIVersion`.
+
+It then goes over all the tracks in the library and checks whether or not they have a `Location`.
+
+If they don’t (which means they are displayed in the Music browser window with a grey warning exclamation mark, and the Music app asks you if you want to try to locate them when you try and play the,) then their details are printed to `stdout`.
+
+If the track does have a `Location` then the path is checked to make sure that there is actually something there.
+
+If the audio file is still missing, then the track details are output to `stdout`.
+
+I originally wrote this utility because I was getting fed up with iTunes (and then the Music app that replaced it) saying that it had successfully imported my audio files, and then only finding out days (or weeks or months or even years) later that in fact it had not, and that the audio files were missing.
+
+(An irritating stunt which seems to be something that even recent versions of Apple Music is prone to pulling off at the most inconvenient moments.)
+
+The app can work both with the current “live” Music library (just call it with no arguments), ie:
+
+`/path/to/list-missing-apple-music-files`
+
+… or you can export your music library as XML from the Music app and then pass the path to that file as an argument, ie:
+
+`/path/to/list-missing-apple-music-files /path/to/my/exported/Library.xml`
+
+[More information on the iTunes Library framework …](https://developer.apple.com/documentation/ituneslibrary)
+
+[More information on the FileManager class …](https://developer.apple.com/documentation/foundation/filemanager)
---
+++ list-missing-apple-music-files/list-missing-apple-music-files/main.m
@@ -0,0 +1,105 @@
+//
+//  main.m
+//  list-missing-apple-music-files
+//
+//  Created by charlie on 16/08/2021.
+//
+
+#import <Foundation/Foundation.h>
+#import <iTunesLibrary/ITLibrary.h>
+#import <iTunesLibrary/ITLibMediaItem.h>
+#import <iTunesLibrary/ITLibArtist.h>
+#import <iTunesLibrary/ITLibAlbum.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+        
+        if (argc == 1)
+        {
+            NSError *error = nil;
+            
+            ITLibrary *library = [ITLibrary libraryWithAPIVersion:@"1.1" error:&error];
+            
+            if (library)
+            {
+                NSFileManager *fileManager = [NSFileManager defaultManager];
+                
+                NSArray<ITLibMediaItem *> *t = library.allMediaItems;
+                
+                NSEnumerator *enumerator = [t objectEnumerator];
+                
+                ITLibMediaItem * item;
+                
+                while (item = [enumerator nextObject])
+                {
+                    ITLibArtist *artist = [item artist];
+                    ITLibAlbum *album = [item album];
+                    
+                    NSString *artistName = [artist name];
+                    NSString *albumTitle = [album title];
+                    
+                    NSString *title = [item title];
+                    
+                    NSURL *mediaItemLocation = [item location];
+                    NSString *pathForFile = [mediaItemLocation path];
+                    
+                    if (mediaItemLocation == NULL)
+                    {
+                        printf("%s - %s - %s\n", [title UTF8String], [artistName UTF8String], [albumTitle UTF8String]);
+                    }
+                    else
+                    {
+                        if (![fileManager fileExistsAtPath:pathForFile])
+                        {
+                            printf("%s - %s - %s - %s\n", [title UTF8String], [artistName UTF8String], [albumTitle UTF8String], [pathForFile UTF8String]);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                printf("%s", [[error localizedDescription] UTF8String]);
+            }
+        }
+        else
+        {
+            NSString * path = [NSMutableString stringWithUTF8String:argv[1]];
+            
+            NSDictionary * music = [NSDictionary dictionaryWithContentsOfFile:path];
+            NSDictionary * tracks = [music valueForKey:@"Tracks"];
+            
+            NSEnumerator * enumerator = [tracks objectEnumerator];
+            
+            NSDictionary * track;
+            
+            while (track = [enumerator nextObject])
+            {
+                NSString * title = [track valueForKey:@"Name"];
+                NSString * artistName = [track valueForKey:@"Artist"];
+                NSString * albumTitle = [track valueForKey:@"Album"];
+                
+                NSURL * location = [NSURL URLWithString:[track valueForKey:@"Location"]];
+                
+                if (!location)
+                {
+                    printf("%s - %s - %s\n", [title UTF8String], [artistName UTF8String], [albumTitle UTF8String]);
+                }
+                else
+                {
+                    NSString * pathForFile = [location path];
+                    
+                    if (![[NSFileManager defaultManager] fileExistsAtPath:pathForFile])
+                    {
+                        printf("%s - %s - %s - %s\n", [title UTF8String], [artistName UTF8String], [albumTitle UTF8String], [pathForFile UTF8String]);
+                    }
+                }
+            }
+        }
+    }
+    
+    return 0;
+}
+
+
+
